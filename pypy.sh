b3cdfe12b8de1d4b6075ae7c55e6aa0f3ff10c67
Guillermo <guillermo.paoletti@gmail.com>
2020-06-13 00:00:18 +0200

    pypy virtualenv helper

---
+++ pypy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~
+mkdir pypy
+cd pypy
+wget https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.3.1-linux64.tar.bz2
+tar xf pypy3.6-v7.3.1-linux64.tar.bz2
+rm pypy3.6-v7.3.1-linux64.tar.bz2
+mv pypy3.6-v7.3.1-linux64/* .
+rm -rf pypy3.6-v7.3.1-linux64
+echo 'virtualenv -p ~/pypy/bin/pypy3 .venvpypy' >> pypy
+chmod +x pypy
+echo 'export PATH="$PATH:~/pypy"' >> ~/.bashrc
