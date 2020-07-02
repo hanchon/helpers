fcfef3bee63cfa7edb1100b63820a2f48d125f1d
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2020-07-02 17:21:52 +0200

    Update pypy.sh

--- pypy.sh
+++ pypy.sh
@@ -7,6 +7,6 @@ tar xf pypy3.6-v7.3.1-linux64.tar.bz2
 rm pypy3.6-v7.3.1-linux64.tar.bz2
 mv pypy3.6-v7.3.1-linux64/* .
 rm -rf pypy3.6-v7.3.1-linux64
-echo 'virtualenv -p ~/pypy/bin/pypy3 .venvpypy' >> pypy
+echo 'virtualenv -p ~/pypy/bin/pypy3 .venv' >> pypy
 chmod +x pypy
 echo 'export PATH="$PATH:~/pypy"' >> ~/.bashrc
